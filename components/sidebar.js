ddd8158d17be50426108cd5d9e3c6e1d0b175a0b
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-14 09:06:46 +0530

    immorted sidebar and navbar

---
+++ components/sidebar.js
@@ -0,0 +1,60 @@
+function sidebar(){
+return ` <div id="leftbox">
+<div>
+  <span class="material-icons iconleft">
+    home
+    </span>
+  <p>Home</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    explore
+    </span>
+  <p>Explore</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    subscriptions
+  </span>
+  <p>Subscriptions</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    video_library
+    </span>
+  <p>Library</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    history
+    </span>
+  <p>History</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    slideshow
+    </span>
+  <p>Your videos</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    watch_later
+    </span>
+  <p>Watch later</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    thumb_up_off_alt
+    </span>
+  <p>Liked videos</p>
+</div>
+<div>
+  <span class="material-icons iconleft">
+    expand_more
+    </span>
+  <p>Show more</p>
+</div>
+</div>`
+}
+
+export default sidebar
